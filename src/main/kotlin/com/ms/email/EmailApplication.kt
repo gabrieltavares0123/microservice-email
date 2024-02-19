1d4fe230748a6c3e6e6043e97358e0d1863cd206
Gabriel Jorge <gabriel.tavaresramos@gmail.com>
2024-02-19 19:41:56 -0300

    Initial commit

---
+++ src/main/kotlin/com/ms/email/EmailApplication.kt
@@ -0,0 +1,11 @@
+package com.ms.email
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmailApplication
+
+fun main(args: Array<String>) {
+	runApplication<EmailApplication>(*args)
+}
